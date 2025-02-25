1522da78455dd1e1997ec77259072822a5114cbc
Edward Angel Lorica <101788598+nngel@users.noreply.github.com>
2025-02-25 07:33:42 +0000

    asd

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 document.getElementById("meaw").onclick = function () {
     document.getElementById("abc").href = "www.google.com";
+    document.getElementById("demo").innerHTML = "Hello JavaScript";
     return false;
 };
